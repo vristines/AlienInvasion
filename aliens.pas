8e0497a32ecfe0a0d456c8559be1a070cff5a6dd
vristines <blazekd93@gmail.com>
2013-10-13 01:07:13 +0200

    Pridani vystupu
    Cas k dosazeni Zeme, doba cesty pro vetrelce

--- aliens.pas
+++ aliens.pas
@@ -1,12 +1,13 @@
 program alien_detector;
 uses crt;
-var d,t,a,v,dist,dist_l,dist_a,dist_acc:int64;
-    vmax:real;
+var d,t,a,v,dist,dist_l,dist_s,dist_acc:int64;
+    vmax,t_s:real;
 const c=299792458;
       ly=9460730472580800;
 
 begin
   t:=0;
+  t_s:=0;
   d:=0;
   v:=0;
   write('Distance of the alien planet (ly) : ');
@@ -18,8 +19,7 @@ begin
   readln(a);
   dist:=dist*ly;
   dist_l:=dist;
-  dist_a:=dist;
-// ZAKLADNI PROGRAM
+  dist_s:=dist;
   repeat
     t:=t+1;
     if (v<vmax) then
@@ -27,19 +27,12 @@ begin
         v:=v+a;
         dist_acc:=dist_acc+v;
       end;
+    t_s:=t_s+(sqrt(1-((v*v)/(c*c))));
     dist_l:=dist_l-c;
-    dist_a:=dist_a-v;
-    {if ((t mod 86400)=trunc(t/86400)) then
-      begin
-        d:=d+1;
-        clrscr;
-        writeln('DAY       ',d);
-        writeln('Velocity : ',v);
-        writeln('Distance (spaceship) : ',dist_a);
-        writeln('Distance (light) : ',dist_l);
-        writeln('Difference : ',dist_a-dist_l);
-      end;}
+    dist_s:=dist_s-v;
   until dist_l=0;
-  writeln('First contact at ',((dist_a-dist_l)/149600000000):5:0,' AU (',((dist_a-dist_l)/1000000000000):5:0,' Gkm)');
+  writeln('First contact at ',((dist_s-dist_l)/149600000000):0:0,' AU (',((dist_s-dist_l)/1000000000000):0:0,' Gkm)');
+  writeln('Minimal time to reach Earth : ',dist_s/v:0:0,' s (',dist_s/v/86400:0:0,' d)');
+  writeln('Travel time of aliens : ',(t_s/86400):0:0,' d)');
   readkey;
 end.
