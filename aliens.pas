50f946264108a227ee4fe8e911c8bc081d994e44
vristines <blazekd93@gmail.com>
2013-10-13 12:31:11 +0200

    Osetreni chyb, uprava vystupu

--- aliens.pas
+++ aliens.pas
@@ -6,33 +6,44 @@ const c=299792458;
       ly=9460730472580800;
 
 procedure init;
+var check:boolean;
 begin
   t:=0;
   t_s:=0;
   d:=0;
   v:=0;
-  write('Distance of the alien planet (ly) : ');
-  readln(dist);
-  write('Travel speed of aliens (c) : ');
-  readln(vmax);
+  repeat
+    write('Distance of the alien planet (ly) : ');
+    readln(dist);
+    if dist>975 then
+      begin
+        writeln('Distance is unfortunately too high, try to set lower value...');
+        delay(2000);
+        check:=false;
+      end
+    else check:=true;
+    vmax:=c*vmax;
+    clrscr;
+  until check=true;
+  repeat
+    write('Travel speed of aliens (c) : ');
+    readln(vmax);
+    if vmax>=1 then
+      begin
+        writeln('Speed must be lower than speed of light - insert number lower than 1');
+        delay(2000);
+        check:=false;
+      end
+    else check:=true;
+    clrscr;
+  until check=true;
   vmax:=c*vmax;
   write('Acceleration of alien spaceship (m/s) : ');
   readln(a);
   dist:=dist*ly;
   dist_l:=dist;
   dist_s:=dist;
-end;
-
-
-procedure bug;
-begin
-clrscr;
-writeln(t);
-writeln(v);
-writeln(t_s);
-writeln(dist_l);
-writeln(dist_s);
-delay(100);
+  clrscr;
 end;
 
 procedure lightcontrol;
@@ -41,11 +52,15 @@ begin
     begin
     diff:=dist_s-dist_l;
     t_r:=diff/v;
+    writeln('Y ',trunc(t/86000/365),' D ',trunc((t mod 86000)/365),' : This is first moment, when we can detect start of alien spaceship.');
     end;
 end;
 
+
+
 procedure starting;
 begin
+  writeln('Y ',trunc(t/86000/365),' D ',trunc((t mod 86000)/365),' : Aliens are leaving their home system.');
   repeat
     t:=t+1;
     v:=v+a;
@@ -58,6 +73,7 @@ end;
 
 procedure cruising;
 begin
+  writeln('Y ',trunc(t/86000/365),' D ',trunc((t mod 86000)/365),' : Alien spaceship reached maximal velocity of ',vmax:0:0,' m/s.');
   repeat
     t:=t+1;
     t_s:=t_s+(sqrt(1-((v*v)/(c*c))));
@@ -69,6 +85,7 @@ end;
 
 procedure stopping;
 begin
+  writeln('Y ',trunc(t/86000/365),' D ',trunc((t mod 86000)/365),' : Alien spaceship is slowing.');
   repeat
     t:=t+1;
     v:=v-a;
@@ -77,6 +94,7 @@ begin
     dist_s:=dist_s-v;
     lightcontrol;
   until v<0;
+  writeln('Y ',trunc(t/86000/365),' D ',trunc((t mod 86000)/365),' : Alien spaceship is here...');
 end;
 
 begin
@@ -84,8 +102,9 @@ begin
   starting;
   cruising;
   stopping;
+  writeln('----------------------------------------------');
   writeln('First contact at ',diff/149600000000:0:0,' AU (',diff/1000000000000:0:0,' Gkm)');
   writeln('Minimal time to reach Earth : ',t_r:0:0,' s (',t_r/86400:0:0,' d)');
-  writeln('Travel time for aliens : ',(t_s/86400):0:0,' d');
+  writeln('Travel time for aliens : ',(t_s):0:0,' s (',(t_s/86400):0:0,' d)');
   readkey;
 end.
